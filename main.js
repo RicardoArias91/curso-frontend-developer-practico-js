9fc880936afc4a9c75ab832bf2b0a8657466c0dd
RicardoArias91 <mantenterdar@gmail.com>
2023-03-07 21:20:16 -0500

    moficando js

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesplHam = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', mostrarMenuDesktop);
 menuDesplHam.addEventListener('click', mostrarMenuMobile);
 menuCarritoIcon.addEventListener('click', mostrarCarritoAside);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside)
 
-function mostrarMenuDesktop() {
+/*function mostrarMenuDesktop() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if (!isAsideClosed) {
       shoppingCartContainer.classList.add('inactive');
@@ -23,19 +26,56 @@ function mostrarMenuMobile() {
     if (!isAsideClosed) {
       shoppingCartContainer.classList.add('inactive');
   }
+  closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 function mostrarCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isMostrarMenuDesktopClosed = desktopMenu.classList.contains('inactive');
+    //const isMostrarMenuDesktopClosed = desktopMenu.classList.contains('inactive');
       if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
-    }else if (!isMostrarMenuDesktopClosed) {
-        desktopMenu.classList.add('inactive')
+    }
+    const isMostrarMenuDesktopClosed = productDetailContainer.classList.contains('inactive');
+
+    if (!isMostrarMenuDesktopClosed) {
+        productDetailContainer.classList.add('inactive')
     }
     shoppingCartContainer.classList.toggle('inactive');
 }
+function openProductDetailAside (){
+  shoppingCartContainer.classList.add('inactive')
+  productDetailContainer.classList.remove('inactive');
+}
+function closeProductDetailAside (){
+  productDetailContainer.classList.add('inactive');
+}
+*/
+function mostrarMenuDesktop() {
+  desktopMenu.classList.toggle("inactive");
+  shoppingCartContainer.classList.add("inactive");
+  productDetailContainer.classList.add("inactive");
+}
+function mostrarMenuMobile() {
+  mobileMenu.classList.toggle("inactive");
+  desktopMenu.classList.add("inactive");
+  productDetailContainer.classList.add("inactive");
+}
+function mostrarCarritoAside() {
+  shoppingCartContainer.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+  productDetailContainer.classList.add("inactive");
 
+}
+function openProductDetailAside (){
+  productDetailContainer.classList.toggle("inactive");
+}
+function closeProductDetailAside (){
+  shoppingCartContainer.classList.toggle("inactive");
+  productDetailContainer.classList.add("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
+}
 const productListArr = [];
 productListArr.push({
   name: 'Bike',
@@ -53,13 +93,15 @@ productListArr.push({
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
 
-
+function renderProducts(arr) {
+  
 for (product of productListArr){
   const productCard = document.createElement('div');
   productCard.classList.add('product-card');
 
   const ProductImg = document.createElement('img');
   ProductImg.setAttribute('src', product.image);
+  ProductImg.addEventListener('click', openProductDetailAside);
 
   const productInfo = document.createElement('div');
   productInfo.classList.add('product-info');
@@ -89,5 +131,7 @@ for (product of productListArr){
   productCard.appendChild(productInfo);
 
   cardsContainer.appendChild(productCard);
+  }
 }
+renderProducts(productListArr);
 
